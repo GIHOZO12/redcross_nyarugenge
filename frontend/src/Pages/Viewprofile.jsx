66c3b17c61c62c899d2957d9948fa83e4aa4f5f3
GIHOZO12 <133333177+GIHOZO12@users.noreply.github.com>
2025-03-07 16:16:56 +0200

    Update Viewprofile.jsx

--- frontend/src/Pages/Viewprofile.jsx
+++ frontend/src/Pages/Viewprofile.jsx
@@ -9,7 +9,7 @@ const Viewprofile = () => {
   const [Profile,setProfile]=useState([])
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/current_user/", {
+    axios.get("https://gihozo.pythonanywhere.com//api/current_user/", {
       withCredentials: true 
     })
     .then((response) => {
