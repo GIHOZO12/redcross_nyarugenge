78fe893b2e7ec7e262d79405208707ba802fde14
Ismail gihozo <gihozoismail@gmial.com>
2025-03-09 11:59:37 +0200

    new update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { Link, useNavigate } from "react-router-dom";
 import { FaChevronDown, FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo.png";
-import Logo1 from '../assets/red cross logo.jpg'
+import Logo1 from '../assets/Red cross logo p.png';
 import profile_icon from "../assets/profile_icon.png";
 import { AppContext } from "../AppContext/Appcontext";
 
