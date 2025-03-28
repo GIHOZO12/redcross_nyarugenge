861956bd89df605ff484e0460d770c68c776763b
Ismail gihozo <gihozoismail@gmial.com>
2025-03-28 16:06:19 +0200

    new one

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,20 +1,26 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useState,useEffect } from "react";
 
 const Footer = () => {
-  
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  // This useEffect isn't strictly necessary since the year changes only once per year,
+  // but it's included in case you want to update it dynamically
+  useEffect(() => {
+    setCurrentYear(new Date().getFullYear());
+  }, []);
 
   return (
     <div className="bg-black relative">
       <hr className="border-white" />
       <div className="p-4">
         <p className="text-center text-white">
-          &copy; Copyright || All rights reserved, designed by{" "}
-          <span className="font-bold text-blue-700 hover:text-blue-500"><a href="https://gihozo12.github.io/my_portofolio/">  IsmailG</a></span>
+          &copy; {currentYear} Copyright || All rights reserved, designed by{" "}
+          <span className="font-bold text-blue-700 hover:text-blue-500">
+            <a href="https://gihozo12.github.io/my_portofolio/">IsmailG</a>
+          </span>
         </p>
       </div>
-
-       
     </div>
   );
 };
